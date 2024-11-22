97ae1556b1652dd97011a01f0a23846eb9bf5df5
Lwandle Chauke <165420337+Lwandle-Chauke@users.noreply.github.com>
2024-11-22 16:11:14 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PROGPart2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
